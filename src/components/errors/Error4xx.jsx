af488967ee393681ca326f896efd24849c0ec553
lee02g29 <lee02g29@naver.com>
2025-11-06 17:35:35 +0900

    Chore: erros 페이지 제거

--- src/components/errors/Error4xx.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Error4xx() {
-  return (
-    <div>Error4xx</div>
-  )
-}
--- src/components/errors/Error5xx.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Error5xx() {
-  return (
-    <div>Error5xx</div>
-  )
-}
