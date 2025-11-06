e42d2f51127d7a38af16c872df1587276e799470
lee02g29 <lee02g29@naver.com>
2025-11-06 17:18:52 +0900

    Feat: http에러 페이지 추가

---
+++ src/components/errors/Error4xx.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Error4xx() {
+  return (
+    <div>Error4xx</div>
+  )
+}
---
+++ src/components/errors/Error5xx.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Error5xx() {
+  return (
+    <div>Error5xx</div>
+  )
+}
