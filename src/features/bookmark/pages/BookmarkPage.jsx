4a005bddd5c620a8566abcac6f34c83b74974366
kimhana415 <khnaa415@gmail.com>
2025-11-12 23:23:32 +0900

    [feat] 상세페이지 안내 탭 ui

--- src/features/bookmark/pages/BookmarkPage.jsx
+++ src/features/bookmark/pages/BookmarkPage.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import { mockPlaces } from '@/mocks/places';
 // 컴포넌트
 import ToTopButton from '@/components/ui/ToTopButton';
-import Typography from '@/components/ui/Typography';
 import PlaceList from '@/features/place/components/PlaceList';
 import EmptyState from '@/components/ui/EmptyState';
+import { useNavigate } from 'react-router-dom';
 
-export default function FavoritePage() {
-  // 장소 토탈 카운트
-  const totalCount = mockPlaces.length || 0;
+export default function BookmarkPage() {
+  const navigate = useNavigate();
+  const totalCount = mockPlaces.length || 0; // 장소 토탈 카운트
+
+  // 장소 클릭
+  const handleClickPlace = (placeId) => {
+    navigate(`/places/${placeId}`);
+  };
 
   return (
     <>
       <ToTopButton />
 
       {totalCount > 0 ? (
-        <PlaceList places={mockPlaces} totalCount={totalCount} />
+        <PlaceList places={mockPlaces} totalCount={totalCount} onClickPlace={handleClickPlace} />
       ) : (
         <EmptyState variant='favorite' />
       )}
--- src/features/placeDetail/containers/PlaceDetailPage.jsx
+++ src/features/placeDetail/containers/PlaceDetailPage.jsx
@@ -1,4 +1,4 @@
-import PlaceDetailContent from '@/features/placeDetail/PlaceDetailContent';
+import PlaceDetailContent from '@/features/placeDetail/components/PlaceDetailContent';
 import React from 'react';
 
 export default function PlaceDetailPage() {
