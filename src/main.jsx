98c0307802ffd114de4459e14925f0c463e9e009
lee02g29 <lee02g29@naver.com>
2025-11-13 10:18:19 +0900

    Feat: 로그인 페이지 UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import '@/styles/index.css';
 import App from './App.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Toaster } from 'react-hot-toast';
 
 // QueryClient 인스턴스 생성
 const queryClient = new QueryClient({
@@ -26,7 +25,6 @@ enableMocking().then(() => {
   createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
       <App />
-      <Toaster position='top-center' reverseOrder={false} />
     </QueryClientProvider>
   );
 });
