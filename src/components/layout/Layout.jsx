d8dee9bf3968d990f261dfd0f33955c9c375d817
kimhana415 <khnaa415@gmail.com>
2025-11-14 13:27:17 +0900

    [refactor] Layout 컴포넌트에서 바텀바 안보이는 페이지 처리

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet, useMatch } from 'react-router-dom';
 // 컴포넌트
 import BottomBar from '@/components/layout/BottomBar';
 import Header from '@/components/layout/Header';
 
 export default function Layout() {
-  // 로그인 페이지 구분
-  const location = useLocation();
-  const isAuthPage = location.pathname === '/login';
+  const hideBottomBar = useMatch('/login') || useMatch('/places/*');
 
   return (
     <div className='bg-stoov-gray-900 relative mx-auto flex min-h-dvh w-full max-w-[500px] flex-col'>
@@ -17,7 +15,7 @@ export default function Layout() {
         <Outlet className='p-1' />
       </main>
 
-      {!isAuthPage && <BottomBar />}
+      {!hideBottomBar && <BottomBar />}
     </div>
   );
 }
