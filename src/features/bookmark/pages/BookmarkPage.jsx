fd82e2bc1fa5414f49200235ce47763c61e0fd65
kimhana415 <khnaa415@gmail.com>
2025-11-21 09:55:03 +0900

    [feat] 관심 장소 저장 버튼 기능 추가

--- src/features/bookmark/pages/BookmarkPage.jsx
+++ src/features/bookmark/pages/BookmarkPage.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { mockPlaces } from '@/mocks/places';
+// 스토어
 import { useAuthStore } from '@/stores/useAuthStore';
+// 쿼리
 import { useBookmarksQuery } from '@/features/bookmark/hooks/useBookmarks';
 // 컴포넌트
 import ToTopButton from '@/components/ui/ToTopButton';
@@ -10,30 +11,29 @@ import EmptyState from '@/components/ui/EmptyState';
 
 export default function BookmarkPage() {
   const navigate = useNavigate();
-  const totalCount = mockPlaces.length || 0; // 장소 토탈 카운트
 
-  // const { userId } = useAuthStore();
-  // const isLoggedIn = !!userId;
+  const { userId } = useAuthStore();
+  const isLoggedIn = !!userId;
 
-  // if (!isLoggedIn) {
-  //   return (
-  //     <EmptyState
-  //       variant='login'
-  //       buttonLabel='로그인 하러 가기'
-  //       onButtonClick={() => navigate('/login')}
-  //     />
-  //   );
-  // }
+  // 관심 장소 리스트 조회
+  const { data: bookmarks, isLoading, isError } = useBookmarksQuery();
 
-  // // 관심 장소 리스트 조회
-  // const { data: bookmarks, isLoading, isError } = useBookmarksQuery();
-  // const totalCount = places.length;
-  // if (isLoading) {
-  //   return <div>관심 장소를 불러오는 중입니다...</div>;
-  // }
-  // if (isError) {
-  //   return <div>오류가 발생했습니다.</div>;
-  // }
+  if (!isLoggedIn) {
+    return (
+      <EmptyState
+        variant='login'
+        buttonLabel='로그인 하러 가기'
+        onButtonClick={() => navigate('/login')}
+      />
+    );
+  }
+
+  if (isLoading) {
+    return <div>관심 장소를 불러오는 중입니다...</div>;
+  }
+  if (isError) {
+    return <div>오류가 발생했습니다.</div>;
+  }
 
   // 장소 클릭
   const handleClickPlace = (placeId) => {
@@ -44,10 +44,9 @@ export default function BookmarkPage() {
     <>
       <ToTopButton />
 
-      {totalCount > 0 ? (
-        <PlaceList places={mockPlaces} totalCount={totalCount} onClickPlace={handleClickPlace} />
+      {places.length > 0 ? (
+        <PlaceList places={places} onClickPlace={handleClickPlace} />
       ) : (
-        // <PlaceList places={places} totalCount={totalCount} onClickPlace={handleClickPlace} />
         <EmptyState variant='bookmark' />
       )}
     </>
